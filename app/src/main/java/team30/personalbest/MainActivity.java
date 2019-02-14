0fd6c4f06b1a3342e415cf850209df8d1ef4afed
asiae <aesmael@ucsd.edu>
2019-02-13 16:18:11 -0800

    implemented start/walk buttons and behavior

--- app/src/main/java/team30/personalbest/MainActivity.java
+++ app/src/main/java/team30/personalbest/MainActivity.java
@@ -3,11 +3,14 @@ package team30.personalbest;
 import android.support.v4.util.Consumer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import java.util.Date;
 
 import team30.personalbest.fitness.FitnessService;
 import team30.personalbest.fitness.FitnessSnapshot;
+import team30.personalbest.fitness.StepType;
 import team30.personalbest.goal.CustomGoalAchiever;
 import team30.personalbest.goal.CustomStepGoal;
 import team30.personalbest.goal.GoalAchiever;
@@ -19,6 +22,15 @@ import team30.personalbest.walk.WalkSteps;
 import team30.personalbest.walk.intentional.IntentionalWalkSteps;
 
 public class MainActivity extends AppCompatActivity {
+    Button startButton;
+    Button endButton;
+    TextView totalRunStepsText;
+    TextView distanceRunText;
+    TextView velocityRunText;
+    double distanceRun;
+    double velocityRun;
+    int totalRunSteps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +42,71 @@ public class MainActivity extends AppCompatActivity {
         final StepGoal stepGoal = new CustomStepGoal(fitnessService);
         final GoalAchiever goalAchiever = new CustomGoalAchiever(stepGoal);
 
+       // Run statistic textview
+       totalRunStepsText = findViewById(R.id.total_run_steps);
+       distanceRunText = findViewById(R.id.distance_run);
+       velocityRunText = findViewById(R.id.velocity_run);
+
+       // Will only show after end run
+       totalRunStepsText.setVisibility(View.INVISIBLE);
+       distanceRunText.setVisibility(View.INVISIBLE);
+       velocityRunText.setVisibility(View.INVISIBLE);
+
+       // Start Walk/Run buttons
+       startButton = findViewById(R.id.button_start);
+       endButton = findViewById(R.id.button_end);
+
+       // endButton invisible/gone in the beginning
+       endButton.setVisibility(View.GONE);
+
+       startButton.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               // Set endButton visible
+               startButton.setVisibility(View.GONE);
+               endButton.setVisibility(View.VISIBLE);
+
+               // Get current walk step number
+               walkSteps.startRecording(null, null);
+
+               // Make sure statistics is hidden
+               totalRunStepsText.setVisibility(View.INVISIBLE);
+               distanceRunText.setVisibility(View.INVISIBLE);
+               velocityRunText.setVisibility(View.INVISIBLE);
+           }
+       });
+
+
+       endButton.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               // Show start button again
+               startButton.setVisibility(View.VISIBLE);
+               endButton.setVisibility(View.GONE);
+
+               // Get current walk step number
+               walkSteps.stopRecording(null, null);
+
+               // Display stats after run
+
+           /*    totalRunSteps = walkSteps.getActiveStats().getTotalSteps(StepType.INTENTIONAL);
+               totalRunStepsText.setText("Total Steps Taken: "+totalRunSteps);
+
+               distanceRun = walkSteps.getActiveStats().getDistanceTravelled();
+               distanceRunText.setText("Total Distance Travelled: "+distanceRun);
+
+               velocityRun = walkSteps.getActiveStats().getMilesPerHour();
+               velocityRunText.setText("Average Velocity: "+velocityRun);
+*/
+               // Show statistics
+               totalRunStepsText.setVisibility(View.VISIBLE);
+               distanceRunText.setVisibility(View.VISIBLE);
+               velocityRunText.setVisibility(View.VISIBLE);
+
+           }
+       });
+
+
         //Initialize goal achiever (this should be called every startup and when step goal changes)
-        goalAchiever.startAchievingGoal();
+     /*   goalAchiever.startAchievingGoal();
 
         //TODO: EVERYTHING BELOW HERE is trash and is just for an example.
         walkSteps.addStepListener(new StepListener() {
@@ -60,6 +135,6 @@ public class MainActivity extends AppCompatActivity {
             public void accept(Integer integer) {
                 //Do something with the progress data...
             }
-        });
+        }); */
     }
 }
