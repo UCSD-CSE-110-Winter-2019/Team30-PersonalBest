c887a87f84ae6091a661856fb448cdeb69d8f8ed
Andrew <andykuo1supergreen@gmail.com>
2019-03-11 17:15:00 -0700

    Removed unused code in MainActivity

--- app/src/main/java/team30/personalbest/MainActivity.java
+++ app/src/main/java/team30/personalbest/MainActivity.java
@@ -1,6 +1,5 @@
 package team30.personalbest;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,12 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.FitnessOptions;
-import com.google.android.gms.fitness.data.DataType;
 
 import java.util.Calendar;
 import java.util.Iterator;
@@ -47,38 +40,11 @@ public class MainActivity extends AppCompatActivity
 	private FitnessWatcher fitnessWatcher;
 	private FitnessGoalAchiever goalAchiever;
 
-//TODO: remove this later
-	public static final int REQUEST_OAUTH_REQUEST_CODE = 0x1001;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		/*
-		final FitnessOptions fitnessOptions = FitnessOptions.builder()
-				.addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-				.addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-				.build();
-
-		if (!GoogleSignIn.hasPermissions(
-				GoogleSignIn.getLastSignedInAccount(this),
-				fitnessOptions))
-		{
-			GoogleSignIn.requestPermissions(
-					this,
-					REQUEST_OAUTH_REQUEST_CODE,
-					GoogleSignIn.getLastSignedInAccount(this),
-					fitnessOptions
-			);
-		}
-		else
-		{
-			updateStepCount();
-			startRecording();
-		}
-		*/
 
 		this.googleFitnessAdapter = new GoogleFitnessAdapter();
 		this.currentClock = new FitnessClock();
@@ -98,32 +64,6 @@ public class MainActivity extends AppCompatActivity
 		this.setupUI();
 
 		this.googleFitnessAdapter.onActivityCreate(this, savedInstanceState);
-	}
-
-	private void startRecording()
-	{
-		GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
-
-		if (googleSignInAccount == null) return;
-
-		Log.d(TAG, "start recording");
-		Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
-				.subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE);
-	}
-
-	private void updateStepCount()
-	{
-		GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
-
-		Log.d(TAG, "Trying to update?");
-		if (googleSignInAccount == null) return;
-
-		Log.d(TAG, "WHAT?");
-		Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
-				.readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
-				.addOnSuccessListener(dataSet ->
-					Log.d(TAG, dataSet.toString())
-				);
 	}
 
 	private void setupUI()
@@ -220,12 +160,6 @@ public class MainActivity extends AppCompatActivity
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
 	{
-		/*
-		if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_OAUTH_REQUEST_CODE)
-		{
-			updateStepCount();
-			startRecording();
-		}*/
 		this.googleFitnessAdapter.onActivityResult(this, requestCode, resultCode, data);
 	}
 
